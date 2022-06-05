cef17538a4f3ca7b9113b5572edb1da44c6a7e97
Edubew <edubew98@gmail.com>
2022-06-05 18:13:10 +0300

    Install webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,18 @@ module.exports ={
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
+    filename: '[name][contenthash].js',
+    clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist')
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
   },
   module: {
     rules: [
