270c664c7979d40b0a98437d1d8dcd349555ae85
Edubew <edubew98@gmail.com>
2022-06-05 18:54:49 +0300

    Install webpack bundle analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 module.exports ={
   mode: 'development',
   entry: {
@@ -10,6 +11,7 @@ module.exports ={
     filename: '[name][contenthash].js',
     clean: true,
   },
+  devtool: 'source-map',
   devServer: {
     static: {
       directory: path.resolve(__dirname, 'dist')
@@ -30,6 +32,16 @@ module.exports ={
           'sass-loader'
         ],
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
     ], 
   },
   plugins: [
@@ -38,5 +50,6 @@ module.exports ={
       filename: 'index.html',
       template: 'src/template.html',
     }),
+    new BundleAnalyzerPlugin(),
   ],
 }
